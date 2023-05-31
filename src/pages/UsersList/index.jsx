ef23c9077623137fc188482f344d9575cb947543
LisandroP57 <lisandropalavecino1@gmail.com>
2023-05-30 21:14:55 -0300

    Agregando listado de usuarios/productos paginado en 5

---
+++ src/pages/UsersList/index.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+import { getUsers } from "../../services/users.service";
+import { Loading } from "../../components/Loading";
+import { Alert, Typography } from "@mui/material";
+import styles from "./index.module.css";
+
+const columns = [
+  { field: 'id', headerName: 'Id', width: 90 },
+  {
+    field: 'name',
+    headerName: 'Nombre',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'email',
+    headerName: 'Email',
+    width: 150,
+    editable: true,
+  },
+];
+
+export const UserList = () => {
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [alert, setAlert] = useState({
+      message: "",
+      type: "",
+      isOpen: false,
+    });
+  
+    useEffect(() => {
+      setIsLoading(true);
+  
+      const fetchUsers = getUsers();
+      fetchUsers
+        .then((usersResponse) => {
+          setUsers(usersResponse.users);
+        })
+        .catch((error) => {
+          console.error(error);
+  
+          setAlert({
+            message: "Ocurrió un error al obtener la información",
+            type: "danger",
+            show: true,
+          });
+        })
+        .finally(setIsLoading(false));
+    }, []);
+  
+    if(isLoading) return <Loading />;
+  
+    return (
+      <section>
+          { alert.isOpen && ( <Alert alert={alert} setAlert={setAlert}/> ) }
+          <Typography paddingLeft={10} variant="h4">Listado de usuarios:</Typography>
+  
+          <Box className={styles.tableContainer}>
+              <DataGrid
+                  rows={users}
+                  columns={columns}
+                  initialState={{
+                  pagination: {
+                      paginationModel: {
+                      pageSize: 3,
+                      },
+                  },
+                  }}
+                  pageSizeOptions={[3]}
+                  checkboxSelection
+                  disableRowSelectionOnClick
+              />
+          </Box>
+      </section>
+    );
+  };
